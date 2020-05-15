046f0ddd1dc1bc1dd61bdb0e15d980c71e214b1b
daeks <daeks@users.noreply.github.com>
2020-05-15 19:22:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,11 @@ if [ "$GIT" != "OFF" ]; then
   else
     git clone $GIT_URL $NODEAPPDIR/
     chown -R $USERNAME:$USERNAME $NODEAPPDIR/
-    chmod -R 744 $NODEAPPDIR && chmod -R 766 $NODETMPDIR
-  fi;
+    chmod -R 744 $NODEAPPDIR
+    if [ -d "$NODETMPDIR" ]; then
+      chmod -R 766 $NODETMPDIR
+    fi
+  fi
 fi
 
 if [ -f "$NODEAPPDIR/index.js" ] && [ -f "$NODECONFIGDIR/config.js" ]; then
