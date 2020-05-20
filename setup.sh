2f7aa7b050659f4acabb660b55064733f7ca2ace
daeks <daeks@users.noreply.github.com>
2020-05-20 16:06:26 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 cd $NODEAPPDIR
-if [ ! -z "$CERT_DOMAINS" ] && [ ! -z "$CERT_EMAIL" ]; then
+if [ ! -z "$CERT_DOMAIN" ] && [ ! -z "$CERT_EMAIL" ]; then
+  a2enmod rewrite
   a2ensite 000-custom-default
-  service apache2 start
 
-  certbot -n certonly --no-self-upgrade --agree-tos --standalone -m $CERT_EMAIL -d $(echo $CERT_DOMAINS | sed 's/,/ -d /')
-  ln -s /etc/letsencrypt/live/$(hostname -f) /etc/letsencrypt/certs
+  certbot certonly --no-self-upgrade --agree-tos --standalone -m $CERT_EMAIL -d $CERT_DOMAIN #$(echo $CERT_DOMAINS | sed 's/,/ -d /')
+  ln -s /etc/letsencrypt/live/$CERT_DOMAIN /etc/letsencrypt/certs
+  
+  a2enmod rewrite
+  a2ensite 000-custom-default
   
   a2enmod ssl
   a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests
   a2ensite 000-custom-default-ssl
   
-  service apache2 restart
+  service apache2 start
 fi
