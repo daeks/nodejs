4c61904b04c07c892410b7929791d373d42b56f7
daeks <daeks@users.noreply.github.com>
2020-05-20 18:00:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 if [ ! -z "$DOMAIN" ] && [ ! -z "$EMAIL" ]; then
-  
   a2enmod rewrite
   a2ensite 000-custom-default
   a2dissite 000-custom-default-backup
@@ -8,16 +7,11 @@ if [ ! -z "$DOMAIN" ] && [ ! -z "$EMAIL" ]; then
   certbot certonly --no-self-upgrade --agree-tos --noninteractive --standalone -m $EMAIL -d $DOMAIN --pre-hook "apache2ctl stop"
   ln -s /etc/letsencrypt/live/$DOMAIN /etc/letsencrypt/certs
   
-  a2enmod rewrite
-  a2ensite 000-custom-default
-  
   a2enmod ssl
-  a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
   a2ensite 000-custom-default-ssl
-  
-  apache2ctl start
 else
-  a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
   a2ensite 000-custom-default-backup
-  apache2ctl start
 fi
+
+a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
+apache2ctl start
