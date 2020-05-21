3ccd1eea5c2a809ff7d7613f0e79f1af61f977c6
daeks <daeks@users.noreply.github.com>
2020-05-21 11:10:56 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 if [ ! -z "$DOMAIN" ] && [ ! -z "$EMAIL" ]; then
   if [ ! -f $CERTBOT_CONF_DIR/live/$DOMAIN/cert.pem ]; then    
-    sudo certbot certonly --no-self-upgrade --agree-tos --noninteractive --standalone \
+    certbot certonly --no-self-upgrade --agree-tos --noninteractive --standalone \
       --work-dir $CERTBOT_WORK_DIR --config-dir $CERTBOT_CONF_DIR --logs-dir $CERTBOT_LOG_DIR \
-      -m $EMAIL -d $DOMAIN --pre-hook "sudo apache2ctl stop"
+      -m $EMAIL -d $DOMAIN --pre-hook "apache2ctl stop"
     
     if [ -f $CERTBOT_CONF_DIR/live/$DOMAIN/cert.pem ]; then
-      sudo a2dissite 000-custom-default
-      sudo rm -f $APACHE_CONF_DIR/sites-available/000-custom-default.conf
+      a2dissite 000-custom-default
+      rm -f $APACHE_CONF_DIR/sites-available/000-custom-default.conf
     
-      sudo a2enmod rewrite && a2enmod ssl
-      sudo a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
+      a2enmod rewrite && a2enmod ssl
+      a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
       
-      sudo a2dissite default-ssl
-      sudo rm -f $APACHE_CONF_DIR/sites-available/default-ssl.conf
+      a2dissite default-ssl
+      rm -f $APACHE_CONF_DIR/sites-available/default-ssl.conf
       
-      sudo a2ensite 000-custom-default-redirect
-      sudo a2ensite 000-custom-default-ssl
+      a2ensite 000-custom-default-redirect
+      a2ensite 000-custom-default-ssl
     else
-      sudo a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
-      sudo a2ensite 000-custom-default
+      a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
+      a2ensite 000-custom-default
     fi
   else
     flags=""
@@ -27,14 +27,14 @@ if [ ! -z "$DOMAIN" ] && [ ! -z "$EMAIL" ]; then
       flags="$flags --force-renewal"
     fi
   
-    sudo certbot renew --no-random-sleep-on-renew --standalone --no-self-upgrade \
+    certbot renew --no-random-sleep-on-renew --standalone --no-self-upgrade \
       --work-dir $CERTBOT_WORK_DIR --config-dir $CERTBOT_CONF_DIR --logs-dir $CERTBOT_LOG_DIR \
-      --pre-hook "sudo apache2ctl stop" --post-hook "sudo apache2ctl start" $flags
+      --pre-hook "apache2ctl stop" --post-hook "apache2ctl start" $flags
   fi
 else
-  sudo a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
-  sudo a2ensite 000-custom-default
+  a2enmod proxy && a2enmod proxy_html && a2enmod proxy_http && a2enmod lbmethod_byrequests && a2enmod headers
+  a2ensite 000-custom-default
 fi
 
-sudo apache2ctl restart
+apache2ctl restart
 
